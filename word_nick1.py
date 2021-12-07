19f29b1cff40f19e32a41026f17fd9b2aa8afd23
Eonji-sw <85453429+Eonji-sw@users.noreply.github.com>
2021-12-07 17:53:39 +0900

    Update word_nick1.py

--- word_nick1.py
+++ word_nick1.py
@@ -19,7 +19,7 @@ class Word_nick:
         self.count = len(self.lst_kor)   # all words count
 
         #print(self.lst_kors)
-        print(self.lst_kor)
+        #print(self.lst_kor)
         #print(self.count)
 
         print('%d words in words_nick.txt' % self.count)
@@ -27,6 +27,14 @@ class Word_nick:
     def randKor(self):
         k = random.randrange(self.count)
         return self.lst_kor[k]   # create word by random
+
+    def randEng(self, cnt_eng):
+        cnt = cnt_eng
+        engStr = string.ascii_letters
+        eng_word = ""
+        for i in range(cnt):
+            eng_word += random.choice(engStr)
+        return eng_word
 
     def randNum(self, cnt_num):
         cnt = cnt_num
@@ -36,5 +44,13 @@ class Word_nick:
             num_word += random.choice(numStr)
         return num_word
 
-if __name__ == '__main__':
-    Word_nick('words_nick.txt')
+    def randPun(self, cnt_pun):
+        cnt = cnt_pun
+        punStr = string.punctuation
+        pun_word = ""
+        for i in range(cnt):
+            pun_word += random.choice(punStr)
+        return pun_word
+
+#if __name__ == '__main__':
+    #Word_nick('words_nick.txt')
