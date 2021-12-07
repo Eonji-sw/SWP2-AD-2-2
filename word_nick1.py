be9cc15c54550c38a4189994c8bc26968127c711
Eonji-sw <85453429+Eonji-sw@users.noreply.github.com>
2021-12-07 16:57:40 +0900

    Create word_nick1.py

---
+++ word_nick1.py
@@ -0,0 +1,40 @@
+import random
+import string
+
+class Word_nick:
+
+    def __init__(self, filename):
+        self.lst_kors = []
+        self.lst_kor = []
+        f = open(filename, 'r')
+        lines = f.readlines()
+        f.close()
+
+        self.count = 0
+        for line in lines:
+            line_kor = line.split()
+            self.lst_kors.append(line_kor)
+
+        self.lst_kor = sum(self.lst_kors, [])   # all words in list of self.lst_kor
+        self.count = len(self.lst_kor)   # all words count
+
+        #print(self.lst_kors)
+        print(self.lst_kor)
+        #print(self.count)
+
+        print('%d words in words_nick.txt' % self.count)
+
+    def randKor(self):
+        k = random.randrange(self.count)
+        return self.lst_kor[k]   # create word by random
+
+    def randNum(self, cnt_num):
+        cnt = cnt_num
+        numStr = string.digits
+        num_word = ""
+        for i in range(cnt):
+            num_word += random.choice(numStr)
+        return num_word
+
+if __name__ == '__main__':
+    Word_nick('words_nick.txt')
