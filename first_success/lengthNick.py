383be6378ce09549592c64b07d9d29c9a54dee9a
Eonji-sw <85453429+Eonji-sw@users.noreply.github.com>
2021-12-11 02:01:24 +0900

    Update lengthNick.py

--- first_success/lengthNick.py
+++ first_success/lengthNick.py
@@ -1,14 +1,14 @@
 import random
 import copy
 
-from mainNick import *
-
 class LengthNick:
 
     def __init__(self):
-        pass
+        self.lst = [0, 0, 0, 0]
+        self.result = [0, 0, 0, 0]
 
     def displayNick(self, userLen, chk):
+        # Create random length of each option
         checked = chk
         cpchecked = copy.deepcopy(checked)
 
@@ -18,31 +18,28 @@ class LengthNick:
                 cpchecked.remove("None")
         totalLen = len(cpchecked)
 
-        lst = [0, 0, 0, 0]
-
         if totalLen == 1:
-            lst[0] = nickLen
+            self.lst[0] = nickLen
         elif totalLen == 2:
-            lst[0] = random.randrange(1, nickLen)
-            lst[1] = nickLen - lst[0]
+            self.lst[0] = random.randrange(1, nickLen)
+            self.lst[1] = nickLen - self.lst[0]
         elif totalLen == 3:
-            lst[0] = random.randrange(1, nickLen - 1)
-            lst[1] = random.randrange(1, nickLen - lst[0])
-            lst[2] = nickLen - lst[0] - lst[1]
+            self.lst[0] = random.randrange(1, nickLen - 1)
+            self.lst[1] = random.randrange(1, nickLen - self.lst[0])
+            self.lst[2] = nickLen - self.lst[0] - self.lst[1]
         elif totalLen == 4:
-            lst[0] = random.randrange(1, nickLen - 2)
-            lst[1] = random.randrange(1, nickLen - lst[0] - 1)
-            lst[2] = random.randrange(1, nickLen - lst[0] - lst[1])
-            lst[3] = nickLen - lst[0] - lst[1] - lst[2]
+            self.lst[0] = random.randrange(1, nickLen - 2)
+            self.lst[1] = random.randrange(1, nickLen - self.lst[0] - 1)
+            self.lst[2] = random.randrange(1, nickLen - self.lst[0] - self.lst[1])
+            self.lst[3] = nickLen - self.lst[0] - self.lst[1] - self.lst[2]
 
-        result = [0, 0, 0, 0]
-
+        # Match option and length
         j = 0
         for i in range(4):
             if checked[i] == "None":
                 continue
             else:
-                result[i] = lst[j]
+                self.result[i] = self.lst[j]
                 j += 1
 
-        return result
+        return self.result
